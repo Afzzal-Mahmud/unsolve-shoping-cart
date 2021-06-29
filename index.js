20c04906519c7913b201434e6db549f42294eb0e
afzal mahmud <afzzalmahmud@gmail.com>
2021-06-29 16:38:16 +0600

    cart functionality added

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const btnAll = document.getElementsByClassName('btn__add__to__cart')
-const 
+const btnAll = document.getElementsByClassName('btn__add__to__cart');
+const cartItemsEach = document.getElementById('cart__items')
 
+//console.log(cartItemsEach)
 for(var i = 0; i<btnAll.length; i++){
     const btn = btnAll[i]
 
     btn.addEventListener('click',function(e){
-        //taking all information to the main product list throw out the btn__add__to__cart
-        //class
+        //taking all information to the main product list throw out the 
         const productDiv = e.target.parentElement
         var title = productDiv.getElementsByClassName('product__title')[0].innerText
         var imgSrc = productDiv.getElementsByClassName('product__image')[0].src;
-        var details = productDiv.getElementsByClassName('product__details')[0].innerText;
+        var price = productDiv.getElementsByClassName('product__price')[0].innerText;
 
-        console.log(title,imgSrc,details)
+        //console.log(title,imgSrc,price)
 
         //passing details on cartDetailsPassed function
 
-        cartDetailsPassed(title,imgSrc,details)
+        cartDetailsPassed(title,imgSrc,price)
     })
 }
 //crating cartDetailsPassed function and passing argument on this
 
-    function cartDetailsPassed(title,imgSrc,details){
-        var cartContainer = document.createElement('ul');
+    function cartDetailsPassed(title,imgSrc,price){
+        var cartContainer = document.createElement('li');
+        cartItemsEach.append(cartContainer);
+        cartContainer.innerHTML= 
+        `
+        <li class="cart__item__each">
+        <h3>${title}</h3>
+        <img src="${imgSrc}" alt="">
+        <span class="price">${price}</span>
+        <button class="btn remove__btn">
+            delete Item
+        </button>
+    </li>
+        `
+cartContainer.getElementsByClassName('remove__btn')[0].addEventListener('click',btnRemove)   
 
     }
 
@@ -33,8 +46,11 @@ const removeCart = document.getElementsByClassName('remove__btn');
 for(var i =0; i<removeCart.length; i++){
     var removeCartItem = removeCart[i];
 
-    removeCartItem.addEventListener('click', function(e){
-       const btnDel = e.target.parentElement;
-       btnDel.remove()
-    })
+    removeCartItem.addEventListener('click',btnRemove)
+}
+
+function btnRemove(e){
+    removeCartItem = e.target.parentElement
+    //console.log(removeCartItem)
+    removeCartItem.remove();
 }
