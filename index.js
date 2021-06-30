3e56a5c3fc6c7f472740f4119e175ce8f3bd0a2d
afzal mahmud <afzzalmahmud@gmail.com>
2021-06-30 11:46:01 +0600

    added functionality on to cart but not working

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const btnAll = document.getElementsByClassName('btn__add__to__cart');
 const cartItemsEach = document.getElementById('cart__items')
+//delet add to cart item
+
+const removeCart = document.getElementsByClassName('remove__btn');
+for (var i = 0; i < removeCart.length; i++) {
+    var removeCartItem = removeCart[i];
+
+    removeCartItem.addEventListener('click', btnRemove)
+}
+function btnRemove(e) {
+    removeCartItem = e.target.parentElement
+    removeCartItem.remove();
+}
 
 //console.log(cartItemsEach)
-for(var i = 0; i<btnAll.length; i++){
+for (var i = 0; i < btnAll.length; i++) {
     const btn = btnAll[i]
 
-    btn.addEventListener('click',function(e){
-        //taking all information to the main product list throw out the 
+    btn.addEventListener('click', function (e) {
+        //taking all information to the main product list throw out the function
         const productDiv = e.target.parentElement
         var title = productDiv.getElementsByClassName('product__title')[0].innerText
         var imgSrc = productDiv.getElementsByClassName('product__image')[0].src;
@@ -14,38 +26,19 @@ for(var i = 0; i<btnAll.length; i++){
 
         //passing details on cartDetailsPassed function
 
-        cartDetailsPassed(title,imgSrc,price)
+        cartDetailsPassed(title, imgSrc, price)
     })
 }
-        //passing argument on this cartDetailsPassed function
+//passing argument on this cartDetailsPassed function
 
-    function cartDetailsPassed(title,imgSrc,price){
-        var cartContainer = document.createElement('li');
-        cartItemsEach.append(cartContainer);
+function cartDetailsPassed(title, imgSrc, price) {
+    var cartContainer = document.createElement('li');
+    cartItemsEach.append(cartContainer);
 
-        /* jodi html er product title ekbar add hoye jay tahole ar add korbe na kintu
-        ei comment kora code ta direct return kore ditese what happend to see please 
-        uncomment the code below*/
-    
-        /*
-        problem code.see what happend on uncomment
-
-        const productTitle = document.getElementsByClassName('product__title')
-        for(var i =0;i<productTitle.length; i++){
-            var noMoreAddBtn = productTitle[i];
-
-            if(noMoreAddBtn.innerText == title){
-                alert('This item is alrady added to your cart')
-                return;
-            }
-
-        }
-        */
-
-        cartContainer.innerHTML= 
+    //problem code see what happend on uncomment
+    cartContainer.innerHTML =
 
         `
-
         <li class="cart__item__each">
         <h3>${title}</h3>
         <img src="${imgSrc}" alt="">
@@ -53,26 +46,46 @@ for(var i = 0; i<btnAll.length; i++){
         <button class="btn remove__btn">
             delete Item
         </button>
-    </li>
+        </li>
 
         `
-cartContainer.getElementsByClassName('remove__btn')[0].addEventListener('click',btnRemove)   
 
+    /*  
+
+     const productTitle = document.getElementsByClassName('product__title')
+     for(var i =0;i<productTitle.length; i++){
+         var noMoreAddBtn = productTitle[i];
+
+         if(noMoreAddBtn.innerText == title){
+             alert('This item is alrady added to your cart')
+             return;
+         }
+
+     }
+     */
+
+    cartContainer.getElementsByClassName('remove__btn')[0].addEventListener('click', btnRemove)
+
+    createLocalCartData(cartContainer);
+    createLocalCartData(cartContainer.innerHTML);
+
+    // let inner =cartContainer.innerHTML;
+    // createLocalCartData(inner);
 }
 
 
-//delet add to cart item
+//creating a local storage to save cart item data
 
-const removeCart = document.getElementsByClassName('remove__btn');
+// let itemms = document.getElementsByClassName('cart__item__each');
+//cart container is li or the hole red box on the cart item
 
-for(var i =0; i<removeCart.length; i++){
-    var removeCartItem = removeCart[i];
-
-    removeCartItem.addEventListener('click',btnRemove)
-}
-
-function btnRemove(e){
-    removeCartItem = e.target.parentElement
-    //console.log(removeCartItem)
-    removeCartItem.remove();
+function createLocalCartData(cartContainer) {
+    let cartItems;
+    if (localStorage.getItem('cartsItems') === null) {
+        cartItems = [];
+    } else {
+        cartItems = JSON.parse(localStorage.getItem(cartItems));
+    }
+    cartItems.push(cartContainer);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
